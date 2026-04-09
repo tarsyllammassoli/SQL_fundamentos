9341d0875002a458cb17893ae5ab17cc33c8332a
tarsyllammassoli <contatotaymassoli@gmail.com>
2026-04-09 15:19:13 -0300

    Salvando anotações

--- main.py
+++ main.py
@@ -10,8 +10,24 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS contas_bancarias (
                cpf TEXT NOT NULL UNIQUE
                )""")
 
-conexao.execute("""INSERT INTO contas_bancarias 
-                (titular, saldo, cpf) VALUES 
-                ('Pedro', 500, '13780196800')""")
+#conexao.execute("""INSERT INTO contas_bancarias 
+#                (titular, saldo, cpf) VALUES 
+#                ('Tay', -398, '31720036588')""")
+
+cursor.execute("""SELECT * FROM contas_bancarias""")
+#cursor.execute("""UPDATE contas_bancarias
+#               SET saldo = -4000
+#               WHERE id = 2""")
+contas = cursor.fetchall()
+print(contas)
+print("\n")
+
+for conta in contas:
+    id, titular, saldo, cpf = conta
+    print(f"""Id: {id}
+Titular: {titular}
+Saldo: {saldo}
+CPF: {cpf}""")
+    print("\n")
 
 conexao.commit()
